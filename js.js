b6bc7236ec713dec387a5b87dbf7baaef4c9f08a
tageronline <155306688+tageronline@users.noreply.github.com>
2023-12-31 15:15:08 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,81 @@
+let ul1 = document.getElementById ('ul1');
+let button1 = document.getElementById ('button1');
+let whats1 = document.getElementById ('whats1');
+
+button1.onclick = function ()  {
+    ul1.style.display = 'block';
+    button1.style.display = 'none';
+    whats1.style.display = 'block'
+}
+
+
+let ul2 = document.getElementById ('ul2');
+let button2 = document.getElementById ('button2');
+let whats2 = document.getElementById ('whats2');
+
+button2.onclick = function ()  {
+    ul2.style.display = 'block';
+    button2.style.display = 'none';
+    whats2.style.display = 'block'
+}
+
+
+
+
+let ul3 = document.getElementById ('ul3');
+let button3 = document.getElementById ('button3');
+let whats3 = document.getElementById ('whats3');
+
+button3.onclick = function ()  {
+    ul3.style.display = 'block';
+    button3.style.display = 'none';
+    whats3.style.display = 'block'
+}
+
+
+let ul4 = document.getElementById ('ul4');
+let button4 = document.getElementById ('button4');
+let whats4 = document.getElementById ('whats4');
+
+button4.onclick = function ()  {
+    ul4.style.display = 'block';
+    button4.style.display = 'none';
+    whats4.style.display = 'block'
+}
+
+
+
+
+let ul5 = document.getElementById ('ul5');
+let button5 = document.getElementById ('button5');
+let whats5 = document.getElementById ('whats5');
+
+button5.onclick = function ()  {
+    ul5.style.display = 'block';
+    button5.style.display = 'none';
+    whats5.style.display = 'block'
+}
+
+
+let ul6 = document.getElementById ('ul6');
+let button6 = document.getElementById ('button6');
+let whats6 = document.getElementById ('whats6');
+
+button6.onclick = function ()  {
+    ul6.style.display = 'block';
+    button6.style.display = 'none';
+    whats6.style.display = 'block'
+}
+
+
+
+
+let ulcartoon = document.getElementById ('ulcartoon');
+let buttoncartoonn = document.getElementById ('buttoncartoon');
+let whatscartoon = document.getElementById ('whatscartoon');
+
+buttoncartoon.onclick = function ()  {
+    ulcartoon.style.display = 'block';
+    buttoncartoon.style.display = 'none';
+    whatscartoon.style.display = 'block'
+}
